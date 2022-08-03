7ba65bb901eb19ac7d3baf4974260dfaf1c5ff11
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-08-03 09:16:08 +0300

    delete old version slider

--- js/script.js
+++ js/script.js
@@ -416,38 +416,4 @@ window.addEventListener('DOMContentLoaded', () => {
             changedCurrentActivityTransform();
         });
     });
-    // showSlides(slideIndex);
-
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
-
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
-
-    //     slides.forEach(item => item.style.display = "none")
-
-    //     slides[slideIndex - 1].style.display = "block";
-
-    //     if(slides.length < 10){
-    //         current.textContent = `0${slideIndex}`;
-    //     }else{
-    //         current.textContent = slideIndex;
-    //     }
-    // }
-
-    // function plusSlides(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // prev.addEventListener('click', () => {
-    //     plusSlides(-1);
-    // });
-
-    // next.addEventListener('click', () => {
-    //     plusSlides(1);
-    // });
-
 });
